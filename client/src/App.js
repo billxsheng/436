1bf390f02c28fbde1e5c5bcd001e7d9d544efce5
Bill Sheng <billxsheng@gmail.com>
2020-03-27 16:18:35 -0400

    it works

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,77 @@
 import React from 'react';
 import './App.css';
-import { Table, TableHead, TableRow, TableCell, TableBody, Button, Switch, FormControlLabel } from '@material-ui/core';
+import { Table, TableHead, TableRow, TableCell, TableBody, Switch, FormControlLabel } from '@material-ui/core';
 
 class App extends React.Component {
 
   state = {
-    positiveListings: {},
-    negativeListings: {},
-    neutralListings: {}
+    positiveListings: [],
+    negativeListings: [],
+    pneuListings: [],
+    nneuListings: [],
+    showPNeutral: false,
+    showNNeutral: false,
   }
 
   componentDidMount() {
     fetch('http://localhost:8080/api/locations/').then((res => res.json())).then((results) => {
-      const positiveListings = {}
-      const negativeListings = {}
-      const neutralListings = {}
+      let positiveListingsMap = {}
+      let negativeListingsMap = {}
+      let neutralListingsMap = {}
       results.forEach((result) => {
         if (result.ls_name.split(' ')[1] === "Positive") {
-          positiveListings[result.ls_name.split(' ')[0]] = result.ls_count;
+          positiveListingsMap[result.ls_name.split(' ')[0]] = result.ls_count;
         } else if (result.ls_name.split(' ')[1] === "Negative") {
-          positiveListings[result.ls_name.split(' ')[0]] = result.ls_count;
+          negativeListingsMap[result.ls_name.split(' ')[0]] = result.ls_count;
         } else {
-          positiveListings[result.ls_name.split(' ')[0]] = result.ls_count;
+          neutralListingsMap[result.ls_name.split(' ')[0]] = result.ls_count;
         }
       })
-      this.setState({ positiveListings, negativeListings, neutralListings })
+
+      let pneuListingsMap = this.sumObjectsByKey(positiveListingsMap, neutralListingsMap);
+      let nneuListingsmap = this.sumObjectsByKey(negativeListingsMap, neutralListingsMap);
+
+      let positiveListings = this.convertMapToSortedList(positiveListingsMap)
+      let negativeListings = this.convertMapToSortedList(negativeListingsMap)
+      let pneuListings = this.convertMapToSortedList(pneuListingsMap)
+      let nneuListings = this.convertMapToSortedList(nneuListingsmap)
+
+      this.setState({ positiveListings, negativeListings, pneuListings, nneuListings })
     });
   }
 
-  getPositiveSentiments() {
-    let arr = []
-    Object.keys(this.state.positiveListings).forEach((key, index, map) => {
-      arr.push({ location: key, count: this.state.positiveListings[key] })
-    })
-    console.log(arr)
-    arr.map((row, index) => {
-      return (
-        <TableRow key={row.ls_name}>
-          <TableCell align="center">{index + 1}</TableCell>
-          <TableCell align="center" component="th" scope="row">
-            {row.location}
-          </TableCell>
-          <TableCell align="center">{row.count}</TableCell>
-        </TableRow>
-      )
-    })
+  convertMapToSortedList = (map) => {
+    let list = [];
+    for(let entry in map) {
+      list.push({location: entry, count: map[entry]});
+    }
+    list.sort((a,b) => {
+      return b.count - a.count;
+    });
+
+    return list;
   }
 
-  getNegativeSentiments() {
-    console.log(this.state.negativeListings)
-    let arr = []
-    Object.keys(this.state.negativeListings).forEach((key, index, map) => {
-      console.log(map)
-      arr.push({ location: key, count: this.state.negativeListings[key] })
-    })
+  sumObjectsByKey = (...objs) => {
+    return objs.reduce((a, b) => {
+      for (let k in b) {
+        if (b.hasOwnProperty(k))
+          a[k] = (a[k] || 0) + b[k];
+      }
+      return a;
+    }, {});
+  }
 
-    arr.forEach((row, counter) => {
-      return (
-        <TableRow key={row.ls_name}>
-          <TableCell align="center">{counter + 1}</TableCell>
-          <TableCell align="center" component="th" scope="row">
-            {row.location}
-          </TableCell>
-          <TableCell align="center">{row.count}</TableCell>
-        </TableRow>
-      )
-    })
+  handlePositiveSwitch = () => {
+    this.setState(prevState => ({
+      showPNeutral: !prevState.showPNeutral
+    }))
+  }
+
+  handleNegativeSwitch = () => {
+    this.setState(prevState => ({
+      showNNeutral: !prevState.showNNeutral
+    }))
   }
 
   render() {
@@ -78,8 +84,8 @@ class App extends React.Component {
             <FormControlLabel
               control={
                 <Switch
-                  // checked={}
-                  // onChange={}
+                  checked={this.state.showPNeutral}
+                  onChange={this.handlePositiveSwitch}
                   name="checkedB"
                   color="primary"
                 />
@@ -95,14 +101,24 @@ class App extends React.Component {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {/* {this.getPositiveSentiments()} */}
-                {Object.keys(this.state.negativeListings).forEach((key, index, map) => (
-                  <TableRow key={key.ls_name}>
+                {this.state.showPNeutral ?
+                this.state.pneuListings.map((row, index) => (
+                  <TableRow key={index}>
                     <TableCell align="center">{index + 1}</TableCell>
                     <TableCell align="center" component="th" scope="row">
-                      {key.location}
+                      {row.location}
                     </TableCell>
-                    <TableCell align="center">{key.count}</TableCell>
+                    <TableCell align="center">{row.count}</TableCell>
+                  </TableRow>
+                ))
+                :
+                this.state.positiveListings.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{index + 1}</TableCell>
+                    <TableCell align="center" component="th" scope="row">
+                      {row.location}
+                    </TableCell>
+                    <TableCell align="center">{row.count}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -114,8 +130,8 @@ class App extends React.Component {
             <FormControlLabel
               control={
                 <Switch
-                  // checked={}
-                  // onChange={}
+                  checked={this.state.showNNeutral}
+                  onChange={this.handleNegativeSwitch}
                   name="checkedB"
                   color="primary"
                 />
@@ -131,16 +147,26 @@ class App extends React.Component {
                 </TableRow>
               </TableHead>
               <TableBody>
-              {this.getNegativeSentiments()}
-                {/* {this.getNegativeSentiments().forEach((row, counter) => (
-                  <TableRow key={row.ls_name}>
-                    <TableCell align="center">{counter + 1}</TableCell>
+                {this.state.showNNeutral ?
+                this.state.nneuListings.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{index + 1}</TableCell>
                     <TableCell align="center" component="th" scope="row">
                       {row.location}
                     </TableCell>
                     <TableCell align="center">{row.count}</TableCell>
                   </TableRow>
-                ))}} */}
+                ))
+                :
+                this.state.negativeListings.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{index + 1}</TableCell>
+                    <TableCell align="center" component="th" scope="row">
+                      {row.location}
+                    </TableCell>
+                    <TableCell align="center">{row.count}</TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
           </div>
